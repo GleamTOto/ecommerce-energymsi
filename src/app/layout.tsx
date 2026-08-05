d2d0c83fd8833de9a23d40376fa22cb00db8b9d8
DanielBohorquez0425 <danielbohorquez0425@gmail.com>
2026-08-05 10:03:41 -0500

    feat: add sonner toast provider and Toaster to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { SessionProvider } from "@/components/providers/SessionProvider";
@@ -38,6 +39,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Toaster position="top-right" richColors closeButton />
           </ThemeProvider>
         </SessionProvider>
       </body>
